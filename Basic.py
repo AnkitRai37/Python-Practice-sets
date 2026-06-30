9a8fe4cbeabf654c7cf406e377243b7f613cd2a8
Ankit Rai <ankitrai4864@gmail.com>
2026-06-30 17:27:07 +0530

    Code

---
+++ Basic.py
@@ -0,0 +1,37 @@
+
+name_2 = "Ankit"
+print(name_2)
+
+value = 3.14
+print(type(value))
+
+str = "25"
+result = int(str)
+print(result)
+
+print (type("hello"))
+
+a = "100"
+b = 50
+print(a + b)
+
+my_tuple = (1, 2, 3)
+my_tuple[0] = 10
+
+print(bool(""))
+print(bool("Python"))
+
+my_set = {1, 2, 2, 3, 4}
+print(my_set)
+
+my_dict = { (1,2): "tuple_key", [3,4]: "list_key" }
+
+val = 5
+print(str(val) + " is a number")
+
+student = {
+    "name": "Ankit",
+    "marks": [85, 90, 92],
+    "active": True
+}
+print(type(student["marks"]))
