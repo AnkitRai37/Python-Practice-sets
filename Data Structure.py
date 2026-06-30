ae121b94a1b5819c34b97037c4e8cf17f01a27ca
Ankit Rai <ankitrai4864@gmail.com>
2026-06-30 17:38:41 +0530

    Add files via upload

---
+++ Data Structure.py
@@ -0,0 +1,36 @@
+list1 = [2, 4, 6, 3, 8, 5]
+print(sum(list1))
+
+list1 = [2, 4, 6, 3, 8, 5]
+print(list1[::-1])
+
+list1 = [2, 4, 6, 3, 8, 5]
+tuple1 = tuple(list1)
+print(tuple1)
+
+tuple1 = (10, 45, 58, 94)
+print(tuple1[2])
+
+list1 = [12, 2, 5, 8, 2, 86]
+set1 = set(list1)
+print(set1)
+
+set1 = {1, 2, 3}
+set1.add(4)
+set1.remove(2)
+print(set1)
+
+student = {"name" : "Ankit", "Age" : 23, "course" : "CSE"}
+print(student)
+
+student = {"name" : "Ankit", "Age" : 23, "course" : "CSE"}
+student["Age"] = 22
+print(student)
+
+list1 = [1,3,2,76,35,27,76,35,75] s   
+print(3 in list1)
+
+student = {"name" : "Ankit", "Age" : 23, "course" : "CSE"}
+print("name" in student)
+
+
